d293486c7b29b9f598679db4e9a180914b48ff8f
Fahmi Ben Salah <fahmii.ben-salah@gmail.com>
2020-11-21 23:01:30 +0100

    Update consumer contract test

--- pact-contract-consumer/src/main/java/com/example/UserServiceClient.java
+++ pact-contract-consumer/src/main/java/com/example/UserServiceClient.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class UserServiceClient {
 
@@ -16,8 +20,18 @@ public class UserServiceClient {
     }
 
     public User getUser(String id) {
-        final User user = restTemplate.getForObject("/users/" + id, User.class);
+        final LinkedHashMap map = (LinkedHashMap) restTemplate.getForObject("/users/" + id, Object.class);
+        final User user = this.promptUser(map);
         Assert.hasText(user.getFullName(), "Name is blank.");
         return user;
+    }
+
+    private User promptUser(Map map) {
+        return User.builder()
+                .id(map.get("id").toString())
+                .fullName(map.get("fullName").toString())
+                .role(UserRole.valueOf(map.get("role").toString()))
+                .permissions((List<Permission>) map.get("permissions"))
+                .build();
     }
 }
--- pact-contract-consumer/src/test/java/com/example/ContractTest.java
+++ pact-contract-consumer/src/test/java/com/example/ContractTest.java
@@ -1,66 +1,105 @@
 package com.example;
 
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.DslPart;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.consumer.junit.PactProviderRule;
-import au.com.dius.pact.consumer.junit.PactVerification;
-import au.com.dius.pact.core.model.RequestResponsePact;
-import au.com.dius.pact.core.model.annotations.Pact;
-import com.jayway.jsonpath.JsonPath;
+import au.com.dius.pact.model.RequestResponsePact;
+import com.example.rules.RandomPortRule;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = "user-service.base-url:http://localhost:${RANDOM_PORT}",
+        classes = UserServiceClient.class)
 public class ContractTest {
 
-    @Rule
-    public PactProviderRule mockProvider = new PactProviderRule("pact-contract-provider", "localhost", 8080, this);
+    @ClassRule
+    public static RandomPortRule randomPort = new RandomPortRule();
 
-    private RestTemplate restTemplate = new RestTemplate();
+    @Rule
+    public PactProviderRuleMk2 provider = new PactProviderRuleMk2("pact-contract-provider", null, randomPort.getPort(), this);
+
+    @Rule
+    public ExpectedException expandException = ExpectedException.none();
+
+    @Autowired
+    private UserServiceClient userServiceClient;
+
+    private static final String FULL_NAME = "Fahmi BEN SALAH";
+    private static final UserRole ROLE = UserRole.ADMIN;
+    private static final List<Permission> PERMISSIONS = new ArrayList<Permission>() {{
+        add(new Permission("001", "permission_001"));
+    }};
 
     /*** This defines the expected interaction for out test ***/
-    @Pact(provider = "pact-contract-provider", consumer = "pact-contract-consumer")
-    public RequestResponsePact createPact(PactDslWithProvider builder) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
+    @Pact(consumer = "pact-contract-consumer")
+    public RequestResponsePact pactUserExists(PactDslWithProvider builder) {
 
         DslPart body = new PactDslJsonBody()
                 .stringType("id", "01")
-                .stringType("fullName", "Fahmi BEN SALAH")
+                .stringType("fullName", FULL_NAME)
+                .stringType("role", ROLE.toString())
                 .eachLike("permissions")
-                .stringType("id", "001")
-                .stringType("name", "permission_001")
+                .stringType("id", PERMISSIONS.get(0).getId())
+                .stringType("name", PERMISSIONS.get(0).getName())
                 .closeArray();
 
         return builder
-                .given("test GET")
-                .uponReceiving("can get user data from user data provider")
-                .path("/api/users/01")
+                .given("User 01 exists")
+                .uponReceiving("A request to /users/01")
+                .path("/users/01")
                 .method("GET")
                 .willRespondWith()
                 .status(200)
-                .headers(headers)
                 .body(body)
                 .toPact();
     }
 
-    @Test
-    @PactVerification(value = "pact-contract-provider", fragment = "createPact")
-    public void runTest() {
-        System.out.println("MOCK provider URL" + mockProvider.getUrl());
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(mockProvider.getUrl() + "/api/users/01", String.class);
-        assertEquals("01", JsonPath.read(responseEntity.getBody(), "$.id"));
-        assertEquals("Fahmi BEN SALAH", JsonPath.read(responseEntity.getBody(), "$.fullName"));
-        //assertEquals(new JSONArray().appendElement(new Permission("001", "permission_001")), JsonPath.read(responseEntity.getBody(), "$.permissions"));
+    @Pact(consumer = "pact-contract-consumer")
+    public RequestResponsePact pactUserDoesNotExist(PactDslWithProvider builder) {
+
+        return builder
+                .given("User 02 does not exist")
+                .uponReceiving("A request to /users/02")
+                .path("/users/02")
+                .method("GET")
+                .willRespondWith()
+                .status(404)
+                .toPact();
     }
 
+    @Test
+    @PactVerification(fragment = "pactUserExists")
+    public void userExists() {
+        final User user = userServiceClient.getUser("01");
 
+        assertThat(user.getFullName()).isEqualTo(FULL_NAME);
+        assertThat(user.getRole()).isEqualTo(ROLE);
+        assertThat(user.getPermissions()).hasSize(1);
+    }
+
+    @Test
+    @PactVerification(fragment = "pactUserDoesNotExist")
+    public void userDoesNotExist() {
+        expandException.expect(HttpClientErrorException.class);
+        expandException.expectMessage("404 Not Found");
+
+        userServiceClient.getUser("02");
+    }
 }
